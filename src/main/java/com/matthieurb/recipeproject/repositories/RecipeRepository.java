3f3ead9394ed1c63913d3ba5c53a6a1e3e9fda01
Matthieu Rambaud <rambaud.matthieu@gmail.com>
2018-08-24 10:12:18 +0200

    Fork to spring guru version of the project

--- src/main/java/com/matthieurb/recipeproject/repositories/RecipeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.matthieurb.recipeproject.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.matthieurb.recipeproject.domain.Recipe;
-
-public interface RecipeRepository extends CrudRepository<Recipe, Long>{
-
-}
